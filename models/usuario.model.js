eb0cc6ae1c9728c209f461ed7b51f386056c836e
Richard Leodan Veliz Bermello <rveliz@ec.krugercorp.com>
2025-02-23 14:27:55 -0500

    Servicios web de users y estudiantes

---
+++ models/usuario.model.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+const db = require("../config/db.json");
+
+const Usuario = db.define("Usuario", {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+module.exports = Usuario;
